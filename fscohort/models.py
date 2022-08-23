882c1858d2efa2814240475c8d8a2977ed82059e
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-23 17:07:38 +0300

    add file

--- fscohort/models.py
+++ fscohort/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+# class Student(models.Model):
+#     first_name = models.models.CharField(('Adı'), max_length=50)
+#     last_name = models.models.CharField(('Soyadı'), max_length=50)
+#     number = models.models.IntegerField(("Numara"))

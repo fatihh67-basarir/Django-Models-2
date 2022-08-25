92bd9807cba375ef22701556d69f1ee490230ab4
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-25 14:45:49 +0300

    fixed

--- fscohort/admin.py
+++ fscohort/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import Student
-# admin.site.register(Student)
+from .models import Student
+admin.site.register(Student)
--- fscohort/models.py
+++ fscohort/models.py
@@ -1,7 +1,7 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
-# class Student(models.Model):
-#     first_name = models.models.CharField(('Adı'), max_length=50)
-#     last_name = models.models.CharField(('Soyadı'), max_length=50)
-#     number = models.models.IntegerField(("Numara"))
+
+class Student(models.Model):
+    first_name = models.CharField('Adı', max_length=50)
+    last_name = models.CharField('Soyadı', max_length=50)
+    number = models.IntegerField('Numara')

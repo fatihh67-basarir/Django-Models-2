62bc81ba6f5dfe452da67718303ae46198665fab
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-25 15:29:48 +0300

    add

--- fscohort/models.py
+++ fscohort/models.py
@@ -5,3 +5,11 @@ class Student(models.Model):
     first_name = models.CharField('Adı', max_length=50)
     last_name = models.CharField('Soyadı', max_length=50)
     number = models.IntegerField('Numara')
+    about = models.TextField('Hakkında', blank=True, null=True)
+    avatar = models.ImageField('Resim', blank=True, null=True, upload_to='media/')
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name} {self.number} "
+
+
+     
